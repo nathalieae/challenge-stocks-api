0fa506a237d060ab064e614ce0521dafcb01ed28
nathalieae <111769395+nathalieae@users.noreply.github.com>
2022-10-10 08:07:34 -0400

    Update README.java

--- submit_stock_app_server/stock_app_server/README.java
+++ submit_stock_app_server/stock_app_server/README.java
@@ -1,5 +1,5 @@
-To run app: run App.java in an IDE of your choice
+To run app: run App.java from version 2 in an IDE of your choice
 -go to following path: src/main/java/com/cg/App.java
--Go to http://localhost:1111/stockmarket/
+-Go to http://localhost:1111/stockmarket/display
 
 for info on the mapping of the endpoints view StockRestController class
